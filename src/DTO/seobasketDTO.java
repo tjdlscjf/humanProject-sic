45a2e0d2aae49077ea8e80db334c259cf3b25f45
95132 <95132@DESKTOP-5902CP5>
2020-09-04 08:51:47 +0900

    DTO 자료

---
+++ src/DTO/seobasketDTO.java
@@ -0,0 +1,41 @@
+package DTO;
+
+public class seobasketDTO {
+	private int seono;
+	private String userid;
+	private String name;
+	private String size2;
+	private int cnt;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSeono() {
+		return seono;
+	}
+	public void setSeono(int seono) {
+		this.seono = seono;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getSize2() {
+		return size2;
+	}
+	public void setSize2(String size2) {
+		this.size2 = size2;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+} 
